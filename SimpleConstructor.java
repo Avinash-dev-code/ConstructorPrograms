73533fb9c5d4342b6e1c240313649eacdc85d1a9
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-05 19:52:56 +0530

    first program of simple constructor added

---
+++ SimpleConstructor.java
@@ -0,0 +1,19 @@
+
+
+
+
+class SimpleConstructor
+
+{
+    SimpleConstructor(){
+        System.out.println("Object is created now");
+
+    }
+    
+
+    public static void main(String[] args) {
+        SimpleConstructor constructor=new SimpleConstructor();
+
+
+    }
+}
